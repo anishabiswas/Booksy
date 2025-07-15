d86dfabfb1ad100f074380472e6d4ad14dd767d0
anishabiswas <anishabiswas64@gmail.com>
2025-07-15 12:05:43 +0530

    installed packages, create a context for firebase

---
+++ src/context/Firebase.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext } from "react";
+import { initializeApp } from "firebase/app";
+
+const FirebaseContext = createContext(null);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDnGPTKQ533tCdrRMONsBKHxuG8bd3IEwM",
+  authDomain: "booksy-e61b6.firebaseapp.com",
+  projectId: "booksy-e61b6",
+  storageBucket: "booksy-e61b6.firebasestorage.app",
+  messagingSenderId: "174567715488",
+  appId: "1:174567715488:web:8a9e889cfa60679dac4fc5",
+};
+s;
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const useFirebase = () => useContext(FirebaseContext); // custom hook(normal fun) to access the state of context
+
+export const FirebaseProvider = (props) => {
+  return <FirebaseContext.Provider>{props.children}</FirebaseContext.Provider>;
+};

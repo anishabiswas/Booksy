96ba1ec2e7b3d143a893827e63f4d71967e6aa6b
anishabiswas <anishabiswas64@gmail.com>
2025-07-16 23:59:02 +0530

    change navbar link to achieve SPA

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,6 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router";
 
 const MyNavbar = () => {
   return (
@@ -17,9 +18,15 @@ const MyNavbar = () => {
         </Navbar.Brand>
         <Navbar.Brand>Booksy</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/book/list">Add Listing</Nav.Link>
-          <Nav.Link href="/book/orders">Orders</Nav.Link>
+          <Nav.Link as={Link} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/book/list">
+            Add Listing
+          </Nav.Link>
+          <Nav.Link as={Link} to="/book/orders">
+            Orders
+          </Nav.Link>
         </Nav>
       </Container>
     </Navbar>

72f1e4681ac0f212eace45144a22477080881042
anishabiswas <anishabiswas64@gmail.com>
2025-07-22 17:46:44 +0530

    updated Detail page

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,14 +1,16 @@
 import { useParams } from "react-router";
 import { useFirebase } from "../context/Firebase";
 import { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
 
 const BookDetailPage = () => {
-  const params = useParams();
+  const params = useParams(); // get an object with id
   const firebase = useFirebase();
   const [book, setBook] = useState(null);
 
   useEffect(() => {
     //returns a promise
+
     firebase.getBookById(params.bookId).then((value) => setBook(value.data()));
   }, []);
 
@@ -18,8 +20,8 @@ const BookDetailPage = () => {
 
   console.log(book);
   return (
-    <div className="container">
-      <h1>Book Detail Page</h1>
+    <div className="container mt-5">
+      <h1 className="text-center">Book Details</h1>
       <h2>{book.name}</h2>
       <img
         src={book.imageURL}
@@ -31,7 +33,8 @@ const BookDetailPage = () => {
       <h1>Owner Details</h1>
       <p>Name: {book.displayName}</p>
       <p>Email: {book.userEmail}</p>
-      <img src={book.photoURL} alt="user photo" />
+      {/* <img src={book.photoURL} alt="user" /> */}
+      <Button variant="success">Buy now</Button>
     </div>
   );
 };
